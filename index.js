af9469261426dd2669f9a452d46446f1b253737d
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-12 16:17:46 +0530

    NPM

--- index.js
+++ index.js
@@ -1,70 +1,7 @@
-const fsPromises = require("fs").promises;
-const path = require("path");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(
-      path.join(__dirname, "files", "lorem.txt"),
-      "utf-8"
-    );
-    console.log(data);
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
 
-   // await fsPromises.unlink(path.join(__dirname, "files", "lorem.txt"));
+console.log(`your id is --`, uuid());
 
-    await fsPromises.writeFile(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      data
-    );
-
-    await fsPromises.appendFile(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      "\n\nNice to meet you"
-    );
-
-    await fsPromises.rename(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      path.join(__dirname, "files", "promiseComplete.txt")
-    );
-
-    const newData = await fsPromises.readFile(
-      path.join(__dirname, "files", "promiseComplete.txt"),
-      "utf-8"
-    );
-    console.log(newData);
-  } catch (error) {
-    console.log(error);
-  }
-};
-fileOps();
-
-// fs.writeFile(
-//   path.join(__dirname, "files", "reply.txt"),
-//   "Nice to meet you",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("Write successfully..");
-
-//     fs.appendFile(
-//       path.join(__dirname, "files", "reply.txt"),
-//       "\n\nYes is is",
-//       (err) => {
-//         if (err) throw err;
-//         console.log("Append successfully..");
-
-//         fs.rename(
-//           path.join(__dirname, "files", "reply.txt"),
-//           path.join(__dirname, "files", "rename.txt"),
-//           (err) => {
-//             if (err) throw err;
-//             console.log("Rename successfully..");
-//           }
-//         );
-//       }
-//     );
-//   }
-// );
-
-process.on("uncaughtException", (err) => {
-  console.err(`there was an uncaught error: ${err}`);
-  process.exit(1);
-});
